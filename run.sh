da4796b378bf78fb2a5ba7c4c2c170fa0da6da34
sheodox <imagamer27@msn.com>
2023-01-14 11:28:59 -0600

    Add stop scripts
    This makes it easier to stop in dev when switching projects

--- run.sh
+++ run.sh
@@ -7,13 +7,23 @@ if [ ! -f ".env" ]; then
 fi
 
 deploy_target=$1
+stop_target=$2
 
 if [[ $deploy_target == "dev" ]]; then
-  docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build
+	docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build
 elif [[ $deploy_target  == "prod" ]]; then
-  docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
+	docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
+elif [[ $deploy_target  == "stop" ]]; then
+	if [[ $stop_target == "dev" ]]; then
+		docker-compose -f docker-compose.yml -f docker-compose.dev.yml stop
+	elif [[ $stop_target  == "prod" ]]; then
+		docker-compose -f docker-compose.yml -f docker-compose.prod.yml stop
+	fi
 else
-  echo "Invalid deployment target specified! Must run as either:"
-  echo "  ./run.sh dev"
-  echo "  ./run.sh prod"
+	echo "Invalid deployment target specified! Must run as either:"
+	echo "  ./run.sh dev"
+	echo "  ./run.sh prod"
+	echo "  ./run.sh stop dev"
+	echo "  ./run.sh stop prod"
 fi
+
