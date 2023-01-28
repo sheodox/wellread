b6396d2d85678d1741fa4119115b3f9017560342
sheodox <imagamer27@msn.com>
2023-01-27 20:04:14 -0600

    Made breadcrumbs look nicer
    They now aren't just using the inline link class, and now are slightly gray but become brighter on hover.

--- src/static/src/lib/Breadcrumbs.svelte
+++ src/static/src/lib/Breadcrumbs.svelte
@@ -1,16 +1,27 @@
-<style>
+<style lang="scss">
 	.divider {
 		color: var(--sx-gray-75);
 	}
+	a {
+		color: var(--sx-gray-50);
+		font-weight: 500;
+		&:hover {
+			color: white;
+		}
+	}
+	.last {
+		color: white;
+		font-weight: 500;
+	}
 </style>
 
-<div class="breadcrumbs f-row f-wrap gap-1 align-items-center">
+<div class="breadcrumbs f-row f-wrap gap-2 align-items-center">
 	{#each links as link, index}
 		{#if index > 0}<span class="sx-font-size-1 divider"><Icon icon="angle-right" variant="icon-only" /></span>{/if}
 		{#if link.href}
-			<a href={link.href} class="inline-link">{link.text}</a>
+			<a href={link.href}>{link.text}</a>
 		{:else}
-			<span>{link.text}</span>
+			<span class="last">{link.text}</span>
 		{/if}
 	{/each}
 </div>
